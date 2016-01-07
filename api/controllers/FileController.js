80db7d38f51397df2c18d2720c0252c54d30af7e
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-01-06 23:37:04 -0600

    File uploader workign fine

---
+++ api/controllers/FileController.js
@@ -0,0 +1,87 @@
+/**
+ * FileController
+ *
+ * @description :: Server-side logic for managing files
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+ module.exports = {
+
+	/**
+	 * `FileController.upload()`
+	 *
+	 * Upload file(s) to the server's disk.
+	 */
+
+	upload: function (req, res) {
+		// e.g.
+		// 0 => infinite
+		// 240000 => 4 minutes (240,000 miliseconds)
+		// etc.
+		//
+		// Node defaults to 2 minutes.
+		res.setTimeout(0);
+
+		req.file('avatar')
+		.upload({
+
+		  // You can apply a file upload limit (in bytes)
+		  // maxBytes: 1000000
+
+		}, function whenDone (err, uploadedFiles) {
+			if (err) return res.serverError(err);
+			else return res.json({
+				files: uploadedFiles,
+				textParams: req.params.all()
+			});
+		});
+	},
+
+	/**
+	 * `FileController.s3upload()`
+	 *
+	 * Upload file(s) to an S3 bucket.
+	 *
+	 * NOTE:
+	 * If this is a really big file, you'll want to change
+	 * the TCP connection timeout.  This is demonstrated as the
+	 * first line of the action below.
+	 */
+	s3upload: function (req, res) {
+		// e.g.
+		// 0 => infinite
+		// 240000 => 4 minutes (240,000 miliseconds)
+		// etc.
+		//
+		// Node defaults to 2 minutes.
+		res.setTimeout(0);
+
+		req.file('avatar').upload({
+			adapter: require('skipper-s3'),
+			bucket: process.env.BUCKET,
+			key: process.env.KEY,
+			secret: process.env.SECRET
+		}, function whenDone(err, uploadedFiles) {
+			if (err) return res.serverError(err);
+			else return res.json({
+				files: uploadedFiles,
+				textParams: req.params.all()
+			});
+		});
+	},
+
+
+	/**
+	 * FileController.download()
+	 *
+	 * Download a file from the server's disk.
+	 */
+	download: function (req, res) {
+		require('fs').createReadStream(req.param('path'))
+		.on('error', function (err) {
+			return res.serverError(err);
+		})
+		.pipe(res);
+	}
+
+};
